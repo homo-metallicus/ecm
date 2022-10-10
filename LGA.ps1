f78078c86d3d7f507118e42f8c7b5030186c8bc6
Romain DECLE <115003401+homo-metallicus@users.noreply.github.com>
2022-10-10 09:55:01 +0200

    Add files via upload

---
+++ LGA.ps1
@@ -0,0 +1,123 @@
+﻿#
+# LGA.ps1 (C) 2022 @homo-metallicus (Romain DECLE)
+# https://github.com/homo-metallicus/moOde-mountstatus
+#
+# This Program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This Program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+Set-ExecutionPolicy -force Unrestricted
+
+$SOURCE="D:\SCAN\lycee-admin\LGA-Compta"
+$DESTINATION="D:\SHARES\EPL\COMMUN EPL\7-GESTION FINANCIERE et PAIES\5-COCWINELLE"
+$CENTRES=[ordered]@{ "00" = "EPL" ; "01" = "LYCEE" ; "02" = "EXPLOITATION AGRICOLE" ; "03" = "CFA" ; "04" = "CFPPA" ; "05" = "EXPLOITATION HORTICOLE" }
+$ANNEE=get-date -Format yyyy
+$POSTES=[ordered]@{ "CA" = "CERTIFICAT ADMINISTRATIF" ; "D" = "DEPENSES" ; "OR" = "ORD REDUC RECETTES" ; "OV" = "ORD VERS DEPENSES" ; "P" = "PENSIONS" ; "R" = "RECETTES" ; "RD" = "REIMPUTATION DEPENSES" ; "RJ" = "REJETS" ; "RR" = "REIMPUTATION RECETTES" ; "S" = "SALAIRES" }
+$OPERATIONS=[ordered]@{ "D" = "DEPENSES" ; "OR" = "ORD REDUC RECETTES" ; "OV" = "ORD VERS DEPENSES" ; "R" = "RECETTES" ; "RJ" = "REJETS" }
+$JOUR=get-date -Format yyyyMMdd
+$i=0
+$j=0
+$ErrorActionPreference="SilentlyContinue"
+
+ Function CreateFolder($path){
+    $global:foldPath=$null
+    foreach($foldername in $path.split("\"))
+    {
+        $global:foldPath+=($foldername+"\")
+        if(!(Test-Path $global:foldPath))
+        {
+            New-Item -ItemType Directory -Path $global:foldPath
+            echo ""
+            echo "Création du dossier $global:foldPath"
+        }
+    }
+}
+
+#Function MoveFiles($src,$dst){
+#    echo $src." : ".$dst."
+#    #Get-ChildItem $src | % -Process { $PDF = $_.Name ; echo "Copie de $PDF vers $dst" ; try { Copy-Item -Force $SOURCE\$PDF $dst ; Remove-Item -Force $SOURCE\$PDF ; Write-Host 'OK' -fore green ; $i++ } catch { Write-Host 'ERROR' -fore red ; $j++ } ; echo "" }
+#}
+
+Stop-Transcript | out-null
+
+$ErrorActionPreference = "Continue"
+
+Start-Transcript -path $SOURCE\Scan_LGA_$JOUR.txt -Force
+
+echo ""
+echo "Copie des scans"
+
+foreach($c in $CENTRES.Keys)
+{
+    $CENTRE = $($CENTRES.item($c))
+    foreach($p in $POSTES.Keys)
+    {
+        $POSTE = $($POSTES.item($p))
+        if ($POSTE -like "PENSIONS")
+        {
+            foreach($o in $OPERATIONS.Keys)
+            {
+                $OPERATION = $($OPERATIONS.item($o))
+                $SRC = "$SOURCE\$c-$p-$o-*.pdf"
+                $DST = "$DESTINATION\ANNEE $ANNEE\$c-$CENTRE\$POSTE\$OPERATION\"
+                CreateFolder $DST
+                Get-ChildItem $SRC | % -Process { $PDF = $_.Name ; echo "Copie de $PDF vers $DST" ; try { Copy-Item -Force $SOURCE\$PDF $DST ; Remove-Item -Force $SOURCE\$PDF ; Write-Host 'OK' -fore green ; $i++ } catch { Write-Host 'ERROR' -fore red ; $j++ } ; echo "" }
+                #MoveFiles($SRC,$DST)
+            }
+        }
+        else
+        {
+            $SRC = "$SOURCE\$c-$p-*.pdf"
+            $DST = "$DESTINATION\ANNEE $ANNEE\$c-$CENTRE\$POSTE\"
+            CreateFolder $DST
+            Get-ChildItem $SRC | % -Process { $PDF = $_.Name ; echo "Copie de $PDF vers $DST" ; try { Copy-Item -Force $SOURCE\$PDF $DST ; Remove-Item -Force $SOURCE\$PDF ; Write-Host 'OK' -fore green ; $i++ } catch { Write-Host 'ERROR' -fore red ; $j++ } ; echo "" }
+            #MoveFiles($SRC,$DST)
+        }
+        #Get-ChildItem $SRC | % -Process { $PDF = $_.Name ; echo "Copie de $PDF vers $DST" ; try { Copy-Item -Force $SOURCE\$PDF $DST ; Remove-Item -Force $SOURCE\$PDF ; Write-Host 'OK' -fore green ; $i++ } catch { Write-Host 'ERROR' -fore red ; $j++ } ; echo "" }
+    }
+}
+
+echo ""
+echo "Copie des contrats"
+
+foreach($c in $CENTRES.Keys)
+{
+    $CENTRE = $($CENTRES.item($c))
+    $SRC_CT = "$SOURCE\CT-$c-*.pdf"
+    $DST_CT = "$DESTINATION\CONTRATS\$c-$CENTRE\"
+    CreateFolder $DST_CT
+    Get-ChildItem $SRC_CT | % -Process { $PDF = $_.Name ; echo "Copie de $PDF vers $DST_CT" ; try { Copy-Item -Force $SOURCE\$PDF $DST_CT ; Remove-Item -Force $SOURCE\$PDF ; Write-Host 'OK' -fore green ; $i++ } catch { Write-Host 'ERROR' -fore red ; $j++ } ; echo "" }
+    #MoveFiles($SRC_CT,$DST_CT)
+}
+
+echo ""
+echo "Copie des RIB"
+
+$SRC_RIB = "$SOURCE\RIB-*.pdf"
+$DST_RIB = "$DESTINATION\RIB\"
+CreateFolder $DST_RIB
+Get-ChildItem $SRC_RIB | % -Process { $PDF = $_.Name ; echo "Copie de $PDF vers $DST_RIB" ; try { Copy-Item -Force $SOURCE\$PDF $DST_RIB ; Remove-Item -Force $SOURCE\$PDF ; Write-Host 'OK' -fore green ; $i++ } catch { Write-Host 'ERROR' -fore red ; $j++ } ; echo "" }
+#MoveFiles($SRC_RIB,$DST_RIB)
+
+echo "Fichiers copiés : $i"
+
+if ( $j -gt 0 )
+{
+    echo "Erreurs de copie : $j"
+}
+
+echo ""
+
+Stop-Transcript
+
+exit
---
+++ RH.ps1
@@ -0,0 +1,98 @@
+﻿#
+# RH.ps1 (C) 2022 @homo-metallicus (Romain DECLE)
+# https://github.com/homo-metallicus/moOde-mountstatus
+#
+# This Program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This Program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+Set-ExecutionPolicy -force Unrestricted
+
+$SOURCE="D:\SCAN\lycee-admin\RH"
+$DESTINATION="D:\SHARES\EPL\COMMUN EPL\6-GRH\4-RESSOURCES HUMAINES (RH)\RH (Dossier par personnel)"
+$CORRESPONDANCE=[ordered]@{}
+$CORRESPONDANCE["adm"]=[ordered]@{ "name" = "Administratif" ; "1" = "CNI" ; "2" = "PC" ; "3" = "Attestations" ; "4" = "RIB" ; "5" = "Etat civil" ; "6" = "Diplômes" ; "7" = "CV" ; "8" = "Casier judiciaire" ; "9" = "Notifications" }
+$CORRESPONDANCE["ctr"]=[ordered]@{ "name" = "Contrat" ; "1" = "Contrats et avenants" ; "2" = "Arrêtés" ; "3" = "Fiches de notation" ; "4" = "Entretiens pro" ; "5" = "CET" ; "6" = "Rapports inspection" }
+$CORRESPONDANCE["med"]=[ordered]@{ "name" = "Médical" ; "1" = "Visites aptitude" ; "2" = "Arrêts" ; "3" = "Visites poste de travail" }
+$CORRESPONDANCE["form"]=[ordered]@{ "name" = "Formation" ; "1" = "Stages de formation" }
+$CORRESPONDANCE["abs"]=[ordered]@{ "name" = "Absence" ; "1" = "Autorisations absence" ; "2" = "Congés enfant malade" ; "3" = "Réunions" ; "4" = "Absences non autorisées" }
+$ANNEE=get-date -Format yyyy
+$JOUR=get-date -Format yyyyMMdd
+$i=0
+$j=0
+$ErrorActionPreference="SilentlyContinue"
+
+ Function CreateFolder($path){
+    $global:foldPath=$null
+    foreach($foldername in $path.split("\"))
+    {
+        $global:foldPath+=($foldername+"\")
+        if(!(Test-Path $global:foldPath))
+        {
+            New-Item -ItemType Directory -Path $global:foldPath
+            echo ""
+            echo "Création du dossier $global:foldPath"
+        }
+    }
+}
+
+Stop-Transcript | out-null
+
+$ErrorActionPreference = "Continue"
+
+Start-Transcript -path $SOURCE\Scan_RH_$JOUR.txt -Force
+
+echo ""
+echo "Copie des scans"
+
+$SRC = "$SOURCE\*.pdf"
+$filesPath = Get-ChildItem $SRC
+ForEach($file in $filesPath){
+    $fileName = $file.Name
+    $shortedFileName = $fileName.Split('.')[0]; #remove the extension
+    $newFileName = $fileName.Split('.')[0]+"_"+$JOUR+"."+$fileName.Split('.')[1]
+    $arr = $shortedFileName.Split('-')
+
+    #extract info
+    $nom = $arr[0]
+    $mainDirectory = $arr[1]
+    $subDirectory = $arr[2]
+
+    $directory = $CORRESPONDANCE[$mainDirectory]["name"]
+    $subdirectory = $CORRESPONDANCE[$mainDirectory][$subDirectory]
+
+    $DST = "$DESTINATION\$nom\$directory\$subdirectory"
+    echo "Copie de $SOURCE\$fileName vers $DST\$newFileName"
+    try {
+        CreateFolder $DST
+        Copy-Item -Force "$SOURCE\$fileName" "$DST\$newFileName"
+        Remove-Item -Force $SOURCE\$fileName
+        Write-Host 'OK' -fore green
+        $i++
+    } catch {
+        Write-Host 'ERROR' -fore red
+        $j++
+    }
+}
+
+echo "Fichiers copiés : $i"
+
+if ( $j -gt 0 ) {
+    echo "Erreurs de copie : $j"
+}
+
+echo ""
+
+Stop-Transcript
+
+exit
